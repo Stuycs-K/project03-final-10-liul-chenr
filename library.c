697af23243277aa8ff84ea7a8ae6a3d0724afda6
lliu40 <lliu40@stuy.edu>
2024-01-04 15:35:08 -0500

    working on add_song

--- library.c
+++ library.c
@@ -1,2 +1,8 @@
 #include "library.h"
+#define N_SONGS 5
 
+void add_song( char* list[N_SONGS], char* name){
+    for( int i = 0; i < N_SONGS; i++){
+        list[i] = 
+    }
+}
--- library.h
+++ library.h
@@ -1,6 +1,13 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
+
+struct song_node {
+    char name[100];
+    struct song_node *next;};
