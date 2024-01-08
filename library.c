36446411517e7ec05af735e40c3d755da06e8a8f
Lala Liu <lliu40@stuy.edu>
2024-01-07 21:44:49 -0500

    added comments to the functions I made in library.c

--- library.c
+++ library.c
@@ -7,11 +7,21 @@ void print_list( struct song_node *n){
 	}
 }
 
+/*
+	Takes in a character buffer, a song name, and a file type
+	Concatenates the song name and the file type together
+	ex: extentsion( buff, "everything_i_own", ".mp3") will store "everything_i_own.mp3" in buff
+*/
 void extension( char* buff, char* name, char* type){
 	strcpy( buff, name);
 	strcat( buff, type);
 }
 
+/*
+	Takes in a linked list, and a song name
+	Creates a linked list with the song name
+	Inserts the new struct in front of the struct in the parameter
+*/
 struct song_node* insert_front( struct song_node *n, char *name){
 	struct song_node* newNode = (struct song_node*) malloc( sizeof( struct song_node));
 	strcpy( newNode->name, name);
@@ -19,11 +29,20 @@ struct song_node* insert_front( struct song_node *n, char *name){
 	return newNode;
 }
 
+/*
+	Takes in a linked list, and a song name
+	Orders the song name into the linked list alphabetically
+*/
 struct song_node* order( struct song_node *n, char *name){
+	
+	//inserts the node in front
 	struct song_node* newNode = insert_front( n, name);
 	
+	//if the newNode is the first node in the list, return newNode
 	if( newNode->next == NULL)
 		return newNode;
+	
+	//if the newNode is in front of the first node in n, return newNode
 	if( strcmp( newNode->name, n->name) < 0)
 		return newNode;
 	
@@ -31,14 +50,24 @@ struct song_node* order( struct song_node *n, char *name){
 	struct song_node* here = n;
 	
 	while( n != NULL){
+		
+		//if the next node is NULL and newNode is after the current node
+		//set the next node to newNode and the node after newNode to NULL
+		//return front
 		if( n->next == NULL && strcmp( newNode->name, n->name) > 0){
 			n->next = newNode;
 			newNode->next = NULL;
 			return front;
 		}
 		
+		//if newNode goes after the current node
+		//set here to be the current node (keeps track of the node prior to the current one)
 		if( strcmp( newNode->name, n->name) > 0)
 			here = n;
+		
+		//if newNode goes before the current node
+		//set the node after the prior node to newNode and newNode's next node to the current node
+		//return front
 		else if( strcmp( newNode->name, n->name) < 0){
 			here->next = newNode;
 			newNode->next = n;
@@ -48,16 +77,26 @@ struct song_node* order( struct song_node *n, char *name){
 	}
 }
 
+/*
+	Takes in a linked list version of the playlist, the name of the playlist, and a song name
+	Add the song to the song_node version of playlist, and then write the whole list to the playlist file
+	Returns the first node of the linked list
+*/
 struct song_node* add_song( struct song_node* p_node, char* playlist, char* name){
+	
+	//inserts the song into the linked list playlist
 	struct song_node* newNode = order( p_node, name);
 	
+	//converts the playlist name to include the .txt extension
 	char pl[ strlen( playlist) + 4];
 	extension( pl, playlist, ".txt");
 	
+	//opens the playlist file
 	// printf( "opening %s\n", playlist);
 	int p_file = open( pl, O_WRONLY | O_TRUNC, 0644);
 	err( p_file, "p_file failed to open");
-		
+	
+	//write the linked list playlist into the playlist file
 	while( newNode != NULL){
 		write( p_file, newNode->name, strlen( newNode->name));
 		write( p_file, "\n", 1);
@@ -68,13 +107,22 @@ struct song_node* add_song( struct song_node* p_node, char* playlist, char* name
 	return newNode;
 }
 
+/*
+	Takes in a char buffer and a playlist name
+	Creates a text file using the playlist name
+*/
 void make_playlist( char* buff, char* playlist){
+	
+	//converts the playlist name to include the .txt extension
 	char pl[ strlen( playlist) + 4];
 	extension( pl, playlist, ".txt");
-	printf( "%s\n", pl);
+	//printf( "%s\n", pl);
 
+	//create the file using the playlist name
 	int file = open( pl, O_CREAT | O_EXCL, 0644);
 	if( file == -1){
+		
+		//if the file already exist, prompt the user for a new name
 		printf( "playlist already exist\n");
 		fgets( buff, 99, stdin);
 		buff[ strlen(buff) - 1] = 0;
@@ -83,16 +131,23 @@ void make_playlist( char* buff, char* playlist){
 		make_playlist( nbuff, buff);
 	}
 	else
+		//copy the playlist name into the char buffer
 		strcpy( buff, playlist);
 	
 	close( file);
 }
 
+/*
+	Takes in a song name
+	Plays the song using mpg123
+*/
 void play_song( char* name){
+	
+	//adds music_library/ and .mp3 onto the song name
 	char song[ 14 + strlen( name) + 4];
 	extension( song, "music_library/", name);
 	extension( song, song, ".mp3");
-	printf( "command: %s\n", song);
+	// printf( "command: %s\n", song);
 	
 	char* cmdargv[1];
 	cmdargv[0] = "mpg123";
@@ -113,6 +168,10 @@ void play_song( char* name){
 	}
 }
 
+/*
+	Takes in a linked list
+	Frees each node in the list
+*/
 struct song_node* free_list( struct song_node *n){
 	while( n != NULL){
 		struct song_node* node = n;
